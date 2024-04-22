cadb870ab15aaa34f45c66e0ebed4da5c165a1b8
TungTvPh81102 <tungtvph46392@fpt.edu.vn>
2024-04-22 14:41:03 +0700

    Install Pre Lint-staged husky

--- src/index.js
+++ src/index.js
@@ -1,34 +1,42 @@
-const path = require("path");
-const express = require("express"); // Required thư viện dependencies trong package
-const morgan = require("morgan");
-const handlebars = require("express-handlebars").engine;
+const path = require('path');
+const express = require('express'); // Required thư viện dependencies trong package
+const morgan = require('morgan');
+const handlebars = require('express-handlebars').engine;
 const app = express(); // Hàm được xây dựng sẵn trong express
 const port = 3000; // Cổng server
 
+// ROUTER
+const route = require('./routes');
+
 // Đường dẫn thư mục public
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Middleware lấy dữ liêu xử lý form được submit bằng phương thức POST
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+);
+
+app.use(express.json());
 
 // HTTP logger
-app.use(morgan("combined")); // Kiểu logger tiêu chuẩn combined
+app.use(morgan('combined')); // Kiểu logger tiêu chuẩn combined
 
 // Template engine
 app.engine(
-  "hbs",
-  handlebars({
-    extname: ".hbs", // config đuôi file .handlebars thành .hbs
-  })
+    'hbs',
+    handlebars({
+        extname: '.hbs', // config đuôi file .handlebars thành .hbs
+    }),
 );
-app.set("view engine", "hbs");
-app.set("views", path.join(__dirname, "resources/views"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
 
-app.get("/news", (req, res) => {
-  res.render("news");
-});
+// Routes init
+route(app);
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+    console.log(`Example app listening on port ${port}`);
 });
---
+++ src/routes/index.js
@@ -0,0 +1,21 @@
+const newsRouter = require('./news');
+const siteRouter = require('./site');
+
+function route(app) {
+    app.use('/news', newsRouter);
+    app.use('/', siteRouter);
+
+    //   // Query parameters
+    //   app.get("/search", (req, res) => {
+    //     console.log(req.query.q); // lấy value được truyền qua URL thực hiện qua phương thức GET: req.query.q
+    //     res.render("search");
+    //   });
+
+    //   // Form data
+    //   app.post("/search", (req, res) => {
+    //     console.log(req.body); // Lấy dữ liệu được gửi qua phương thức POST: req.body
+    //     res.render("search");
+    //   });
+}
+
+module.exports = route;
